4680280c4c99aae30f73d022d102fa6c2d870881
NORA <noracyh15@gmail.com>
2025-05-02 20:45:24 +0800

    ex. 2025/05/02 test用

--- src/main/java/user/buy/controller/Test.java
+++ src/main/java/user/buy/controller/Test.java
@@ -13,6 +13,6 @@ public class Test extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("大家好");
+		System.out.println("OOOOOOOOOOOO NOOOOOOOOOOO");
 	}
 }
