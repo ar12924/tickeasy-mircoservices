0373b254a4e47c1fa87e374b4150e40ef6e8f8a0
yiwei <willyos0113@gmail.com>
2025-05-31 15:51:04 +0800

    購票頁: 排除按鈕效果bug

--- src/main/webapp/user/buy/js/bookTickets.js
+++ src/main/webapp/user/buy/js/bookTickets.js
@@ -1,23 +1,32 @@
 // ====== section 主內容 ======
-// ------ 上/下一步按鈕區塊 ------
-$(".back").on("click", () => {
-  location.href = "https://www.google.com";
-});
-$(".next").on("click", () => {
-  // post 東西出去(under constructing...)
-  const inputsValues = $(".count")
-    .map((index, el) => {
-      const coreParent = $(el).closest(".level");
-      const typeName = coreParent.find(".type-name").text();
-      const typePrice = coreParent.find(".type-price").text();
-      // 模擬 json 格式
-      return {
-        typeCount: $(el).val(),
-        typeName: typeName,
-        typePrice: typePrice,
-      };
-    })
-    .get();
-  console.log(inputsValues);
-  location.href = "bookDetails.html";
-});
+const bookTicketsJSLoader = () => {
+  $(".update").on("mouseenter mouseleave", (e) => {
+    $(e.target).closest(".update").toggleClass("is-focused");
+  });
+  // ------ 上/下一步按鈕區塊 ------
+  $(".back").on("click", () => {
+    location.href = "https://www.google.com";
+  });
+  $(".next").on("mouseenter mouseleave", (e) => {
+    $(e.target).toggleClass("is-focused");
+  });
+  $(".next").on("click", () => {
+    // post 東西出去(under constructing...)
+    const inputsValues = $(".count")
+      .map((index, el) => {
+        const coreParent = $(el).closest(".level");
+        const typeName = coreParent.find(".type-name").text();
+        const typePrice = coreParent.find(".type-price").text();
+        // 模擬 json 格式
+        return {
+          typeCount: $(el).val(),
+          typeName: typeName,
+          typePrice: typePrice,
+        };
+      })
+      .get();
+    console.log(inputsValues);
+    location.href = "bookDetails.html";
+  });
+};
+bookTicketsJSLoader(); // 載入 JS 事件監聽
