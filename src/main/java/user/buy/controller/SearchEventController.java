40b974cbb506f2fcb3ba6be92b66ee2de700124f
yiwei <willyos0113@gmail.com>
2025-07-03 19:28:29 +0800

    首頁: 動態插入元素，帶有關鍵字、連結，測試跳轉成功。

--- src/main/java/user/buy/controller/SearchEventController.java
+++ src/main/java/user/buy/controller/SearchEventController.java
@@ -1,57 +1,44 @@
 package user.buy.controller;
 
-import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.google.gson.Gson;
-
-import common.util.CommonUtil;
-import common.vo.Core;
 import user.buy.service.SearchService;
 import user.buy.vo.EventInfo;
+import user.buy.vo.KeywordCategory;
 
-/**
- * 首頁(index.html)熱門活動區塊資料界接 
- * 搜尋頁(search.html)活動區塊資料界接 
- * 請求方法: GET 
- * 回應格式: JSON 
- * 資料型態: Core<EventInfo>
- * API: /search-event?keyword="台北"&pageNumber=1&pageSize=3
- */
-@WebServlet("/search-event")
-public class SearchEventController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+@RestController
+@RequestMapping("search-event")
+public class SearchEventController {
+	@Autowired
 	private SearchService service;
 
-	// DL 方式注入(暫時性，後續 spring-MVC 會改)
-	@Override
-	public void init() throws ServletException {
-		service = CommonUtil.getBean(getServletContext(), SearchService.class);
+	/**
+	 * 查詢近期 9 筆活動資料。
+	 * 
+	 * @return {EventInfo} 回應近期 9 筆活動資料。
+	 */
+	@CrossOrigin(origins = "*")
+	@GetMapping("recent")
+	public List<EventInfo> getRecentEventInfo() {
+		Integer rowNumber = 9; // 9筆近期
+		return service.getRecentEventInfo(rowNumber);
 	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 1. 指定允許所有網域
-		resp.setHeader("Access-Control-Allow-Origin", "*");
-		// 2. 接受前端查詢 (keyword, pageNumber, pageSize)
-		String keyword = req.getParameter("keyword");
-		Integer pageNumber = Integer.parseInt(req.getParameter("pageNumber"));
-		Integer pageSize = 6; // 固定一頁6筆
-		// 3. 將 (keyword, pageNumber, pageSize) 交給 Service 處理，回傳查詢結果
-		Core<List<EventInfo>> eventCore = service.searchEventByKeyword(keyword, pageNumber, pageSize);
-		// 4. 轉成 json 格式，並回應 json 字串
-		Gson gson = new Gson();
-		String jsonData = gson.toJson(eventCore);
-		resp.setContentType("application/json");
-		resp.setCharacterEncoding("utf-8");
-		PrintWriter pw = resp.getWriter();
-		pw.print(jsonData);
+	
+	/**
+	 * 透過 keywordId 查詢關鍵字名稱。
+	 * 
+	 * @return {KeywordCategory} 回應對應關鍵字資料。
+	 */
+	@CrossOrigin(origins = "*")
+	@GetMapping("keyword/{keywordId}")
+	public KeywordCategory getKeyword(@PathVariable Integer keywordId) {
+		return service.getKeyword(keywordId);
 	}
 }
--- src/main/java/user/buy/service/SearchService.java
+++ src/main/java/user/buy/service/SearchService.java
@@ -3,32 +3,13 @@ package user.buy.service;
 import java.util.List;
 
 import common.vo.Core;
-import user.buy.vo.BuyerTicket;
 import user.buy.vo.EventInfo;
-import user.buy.vo.MemberNotification;
+import user.buy.vo.KeywordCategory;
 
 public interface SearchService {
-	/**
-	 * 傳入關鍵字，驗證使用者輸入的關鍵字格式
-	 * 
-	 * @param 關鍵字, 頁數, 每頁筆數
-	 * @return 符合條件的數筆活動資料
-	 */
-	Core<List<EventInfo>> searchEventByKeyword(String keyword, Integer pageNumber, Integer pageSize);
+	public List<EventInfo> getRecentEventInfo(Integer n);
 
-	/**
-	 * ...
-	 * 
-	 * @param 無
-	 * @return 符合條件的數筆票券資料
-	 */
-	List<BuyerTicket> searchTicket();
+	public KeywordCategory getKeyword(Integer keywordId);
 
-	/**
-	 * ...
-	 * 
-	 * @param 無
-	 * @return 符合條件的數筆通知資料
-	 */
-	List<MemberNotification> searchNotification();
+	public Core<List<EventInfo>> searchEventByKeyword(String keyword, Integer pageNumber, Integer pageSize);
 }
