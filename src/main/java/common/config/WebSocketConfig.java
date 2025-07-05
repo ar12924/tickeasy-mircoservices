6a669d5c91513570906785564dea26a6fde3c70a
evelyncute7 <dazzling_seed@hotmail.com>
2025-07-05 15:44:10 +0800

    通知中心: WebSocket通知推播的config檔

---
+++ src/main/java/common/config/WebSocketConfig.java
@@ -0,0 +1,32 @@
+package common.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import user.notify.websocket.NotifyWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+	@Autowired
+    private NotifyWebSocketHandler notifyWebSocketHandler;
+
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(notifyWebSocketHandler, "/notify/notification")
+				/*.setAllowedOrigins("*");*/
+				.setAllowedOrigins("http://localhost:8080");
+	}
+
+	/*
+	@Bean
+	public WebSocketHandler webSocketHandler() {
+		return notifyWebSocketHandler;
+	}*/
+}
