a13742414b895b1913447a0de2034a14082c312d
nora <noracyh15@gmail.com>
2025-08-14 16:58:17 +0800

    feat(notification): 初始化 Notification 服務啟動類別

---
+++ src/main/java/microservices/notification/NotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package microservices.notification;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class NotificationServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
+}
+
+
